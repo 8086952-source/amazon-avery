8b21726df667429b35d695973e9f0b12c5dbce80
8086952-source <8086952@student.harmonytx.org>
2025-12-16 13:57:41 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,73 @@
+input.onButtonPressed(Button.A, function () {
+    Passcode = "" + Passcode + "A"
+})
+input.onButtonPressed(Button.B, function () {
+    Passcode = "" + Passcode + "B"
+})
+let Passcode = ""
+basic.clearScreen()
+servos.P0.setAngle(0)
+Passcode = ""
+pins.digitalWritePin(DigitalPin.P1, 0)
+basic.forever(function () {
+    if (Passcode.length == 4) {
+        if (Passcode == "BABA") {
+            for (let index = 0; index < 5; index++) {
+                basic.clearScreen()
+                basic.showLeds(`
+                    # . # . .
+                    . # # # .
+                    . . # . #
+                    . # . # .
+                    # . . . #
+                    `)
+                basic.pause(200)
+                basic.showLeds(`
+                    . . # . #
+                    . # # # .
+                    # . # . .
+                    . # . # .
+                    . # . # .
+                    `)
+                basic.pause(200)
+            }
+            basic.clearScreen()
+            servos.P0.setAngle(90)
+            basic.pause(5000)
+            servos.P0.setAngle(0)
+            while (pins.digitalReadPin(DigitalPin.P1) != 1) {
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+                basic.pause(500)
+            }
+            if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                servos.P0.setAngle(90)
+                basic.pause(5000)
+                servos.P0.setAngle(0)
+            }
+            Passcode = ""
+        } else {
+            basic.clearScreen()
+            music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+            basic.pause(2000)
+            basic.clearScreen()
+            Passcode = ""
+        }
+    }
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
